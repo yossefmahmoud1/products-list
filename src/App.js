850b6bcf71deeb2f1f08e1b26004477bf47013ac
yossefmahmoud1 <163425440+yossefmahmoud1@users.noreply.github.com>
2025-02-09 23:11:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Items from './components/item/items'
 import AddItem from './components/addItem/addItem'
 import Total from './components/total/total'
@@ -7,26 +7,34 @@ import Total from './components/total/total'
 class App extends Component {
   state = {
     items: [
-      {id:1, product:'Pen', price:2},
-      {id:2, product:'Book', price:10}
+      {id: 1, product: 'Pen', price: 2, quantity: 1},
+      {id: 2, product: 'Book', price: 10, quantity: 1}
     ]
   }
 
   deleteItem = (id) => {
-    let items = this.state.items
-    let i = items.findIndex(item => item.id === id)
-    items.splice(i, 1)
-    this.setState({items: items})
+    let items = this.state.items;
+    let i = items.findIndex(item => item.id === id);
+    items.splice(i, 1);
+    this.setState({ items: items });
   }
 
   addItem = (item) => {
-    this.state.items.length > 0 ? (
-      item.id = this.state.items[this.state.items.length - 1].id + 1 
-    ) : item.id = 1
-    console.log(item.id)
-    let items = this.state.items
-    items.push(item)
-    this.setState({items: items})
+    item.id = this.state.items.length > 0 ? this.state.items[this.state.items.length - 1].id + 1 : 1;
+    let items = this.state.items;
+    items.push(item);
+    this.setState({ items: items });
+  }
+
+  updateQuantity = (id, type) => {
+    let items = [...this.state.items];
+    let i = items.findIndex(item => item.id === id);
+    if (type === 'increase') {
+      items[i].quantity += 1;
+    } else if (type === 'decrease' && items[i].quantity > 1) {
+      items[i].quantity -= 1;
+    }
+    this.setState({ items: items });
   }
 
   render() {
@@ -34,9 +42,9 @@ class App extends Component {
       <div className="container">
         <h1>Product List React App</h1>
         <div className="table">
-          <Items items={this.state.items} del={this.deleteItem}/>
-          <AddItem add={this.addItem}/>
-          <Total items={this.state.items}/>
+          <Items items={this.state.items} del={this.deleteItem} updateQuantity={this.updateQuantity} />
+          <AddItem add={this.addItem} />
+          <Total items={this.state.items} />
         </div>
       </div>
     )
